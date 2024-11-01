8d2a5799f576b3c451183bff38c4f8a937cfb5f7
Nedelin222 <anedelin222@gmail.com>
2024-11-01 18:43:32 +0300

    Update DP_modyl.py

--- DP_modyl.py
+++ DP_modyl.py
@@ -1,6 +1,6 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron',}
-gpa = str((sum(grades[0])) / len((grades[0])))
+gpa = float((sum(grades[0])) / len((grades[0])))
 gpa1 = (sum(grades[1])) / len((grades[1]))
 gpa2 = (sum(grades[2])) / len((grades[2]))
 gpa3 = (sum(grades[3])) / len((grades[3]))
